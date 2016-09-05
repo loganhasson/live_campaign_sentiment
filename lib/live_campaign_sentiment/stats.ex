115b8e3d739e6e46c057d039ab5d8eb582f73445
Logan Hasson <logan.hasson@gmail.com>
2016-09-04 20:28:20 -0400

    Some spacing

--- lib/live_campaign_sentiment/stats.ex
+++ lib/live_campaign_sentiment/stats.ex
@@ -1,9 +1,11 @@
 defmodule LiveCampaignSentiment.Stats do
   def empty do
     stored_stats = RedisHash.new("sentiment_stats")
+
     case RedisHash.pull(stored_stats) do
       %{} ->
         %{trump: %{count: 0, average: 0, total: 0, positive: 0, negative: 0}, clinton: %{count: 0, average: 0, total: 0, positive: 0, negative: 0}}
+
       stored_results -> stored_results
     end
   end
