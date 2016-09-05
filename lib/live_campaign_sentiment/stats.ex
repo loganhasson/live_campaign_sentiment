6751c4eeb80031edb46aa692a0074f46dfc1beab
Logan Hasson <logan.hasson@gmail.com>
2016-09-05 16:25:56 -0400

    Prep data structure for rolling averages

--- lib/live_campaign_sentiment/stats.ex
+++ lib/live_campaign_sentiment/stats.ex
@@ -4,7 +4,7 @@ defmodule LiveCampaignSentiment.Stats do
 
     case RedisHash.pull(stored_stats) do
       %{} ->
-        %{trump: %{count: 0, average: 0, total: 0, positive: 0, negative: 0}, clinton: %{count: 0, average: 0, total: 0, positive: 0, negative: 0}}
+        %{trump: %{count: 0, average: 0, total: 0, positive: 0, negative: 0, history: %{current_period: %{}, previous_summaries: []}}, clinton: %{count: 0, average: 0, total: 0, positive: 0, negative: 0, history: %{current_period: %{}, previous_summaries: []}}}
 
       stored_results -> stored_results
     end
