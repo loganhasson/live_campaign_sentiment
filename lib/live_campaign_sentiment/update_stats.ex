86f3649903fbe15fdc27badb4b878dbbbdd6dc69
Logan Hasson <logan.hasson@gmail.com>
2016-09-06 20:55:21 -0400

    Make strings, you know, strings

--- lib/live_campaign_sentiment/update_stats.ex
+++ lib/live_campaign_sentiment/update_stats.ex
@@ -47,19 +47,19 @@ defmodule LiveCampaignSentiment.UpdateStats do
 
   defp get_state_abbreviation(state_name) do
     state_mappings = %{
-      "Alabama" => AL, "Alaska" => AK, "Arizona" => AZ, "Arkansas" => AR,
-      "California" => CA, "Colorado" => CO, "Connecticut" => CT, "Delaware" => DE,
-      "Florida" => FL, "Georgia" => GA, "Hawaii" => HI, "Idaho" => ID,
-      "Illinois" => IL, "Indiana" => IN, "Iowa" => IA, "Kansas" => KS,
-      "Kentucky" => KY, "Louisiana" => LA, "Maine" => ME, "Maryland" => MD,
-      "Massachusetts" => MA, "Michigan" => MI, "Minnesota" => MN,
-      "Mississippi" => MS, "Missouri" => MO, "Montana" => MT, "Nebraska" => NE,
-      "Nevada" => NV, "New Hampshire" => NH, "New Jersey" => NJ, "New Mexico" => NM,
-      "New York" => NY, "North Carolina" => NC, "North Dakota" => ND, "Ohio" => OH,
-      "Oklahoma" => OK, "Oregon" => OR, "Pennsylvania" => PA, "Rhode Island" => RI,
-      "South Carolina" => SC, "South Dakota" => SD, "Tennessee" => TN, "Texas" => TX,
-      "Utah" => UT, "Vermont" => VT, "Virginia" => VA, "Washington" => WA,
-      "West Virginia" => WV, "Wisconsin" => WI, "Wyoming" => WY
+      "Alabama" => "AL", "Alaska" => "AK", "Arizona" => "AZ", "Arkansas" => "AR",
+      "California" => "CA", "Colorado" => "CO", "Connecticut" => "CT", "Delaware" => "DE",
+      "Florida" => "FL", "Georgia" => "GA", "Hawaii" => "HI", "Idaho" => "ID",
+      "Illinois" => "IL", "Indiana" => "IN", "Iowa" => "IA", "Kansas" => "KS",
+      "Kentucky" => "KY", "Louisiana" => "LA", "Maine" => "ME", "Maryland" => "MD",
+      "Massachusetts" => "MA", "Michigan" => "MI", "Minnesota" => "MN",
+      "Mississippi" => "MS", "Missouri" => "MO", "Montana" => "MT", "Nebraska" => "NE",
+      "Nevada" => "NV", "New Hampshire" => "NH", "New Jersey" => "NJ", "New Mexico" => "NM",
+      "New York" => "NY", "North Carolina" => "NC", "North Dakota" => "ND", "Ohio" => "OH",
+      "Oklahoma" => "OK", "Oregon" => "OR", "Pennsylvania" => "PA", "Rhode Island" => "RI",
+      "South Carolina" => "SC", "South Dakota" => "SD", "Tennessee" => "TN", "Texas" => "TX",
+      "Utah" => "UT", "Vermont" => "VT", "Virginia" => "VA", "Washington" => "WA",
+      "West Virginia" => "WV", "Wisconsin" => "WI", "Wyoming" => "WY"
     }
 
     case state_mappings[state_name] do
