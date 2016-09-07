579edf24a99a3a9c34f01164c588ba95b7b86aa4
Logan Hasson <logan.hasson@gmail.com>
2016-09-07 00:33:11 -0400

    Don't sent huge array of tweets to front end

--- lib/live_campaign_sentiment/store_and_publish_stats.ex
+++ lib/live_campaign_sentiment/store_and_publish_stats.ex
@@ -16,12 +16,33 @@ defmodule LiveCampaignSentiment.StoreAndPublishStats do
   end
 
   defp publish(state = %{trump: %{count: trump_count}, clinton: %{count: clinton_count}}) when rem((trump_count + clinton_count), 10) == 0 do
-    LiveCampaignSentiment.Endpoint.broadcast("sentiment:global", "global_sentiment_update", state)
+    LiveCampaignSentiment.Endpoint.broadcast("sentiment:global", "global_sentiment_update", lite_state(state))
 
     :ok
   end
 
-  defp publish(state) do
+  defp publish(_) do
     :ok
+  end
+
+  defp lite_state(%{trump: trump = %{rolling: trump_rolling, location: trump_location}, clinton: clinton = %{rolling: clinton_rolling, location: clinton_location}}) do
+    %{
+      trump: %{
+        trump | rolling: %{trump_rolling | tweets: %{}}, location: lite_location(trump_location)
+      },
+      clinton: %{
+        clinton | rolling: %{clinton_rolling | tweets: %{}}, location: lite_location(clinton_location)
+      }
+    }
+  end
+
+  defp lite_location(location_data) do
+    for {key, value} <- location_data do
+      new_value = for {sub_key, sub_value} <- value, sub_key != :tweets do
+        {sub_key, sub_value}
+      end |> Enum.into(%{})
+
+      {key, new_value}
+    end |> Enum.into(%{})
   end
 end
